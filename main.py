770bffee1f8d7038b4ff9dd6c8b8b2a5b7a28dbc
kimdy03 <60499120+kimdy03@users.noreply.github.com>
2020-02-27 23:16:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,23 @@ access_token = os.environ["BOT_TOKEN"]
 @client.event
 async def on_ready () : # 항상
       print(client.user.name,"실행중")
-      activity = discord.Game(name="-문의 (문의 내용) 으로 문의/건의사항 을 보내주세요~!")
+      activity = discord.Game(name="저에게 문의내용을 보내주세요!!")
       await client.change_presence(status=discord.Status.online, activity=activity) # idle = 자리비움
 
+contents = ""
 @client.event
 async def on_message(message) :
-    if message.content.startswith("-문의") : 
-        msg = message.content[4:]
-        author = message.guild.get_member(337849294591885322)
-        await author.send(msg)
-        await message.channel.send("해당 사항이 전달되었습니다.")
+    if isinstance(message.channel,discord.DMChannel):
+        if message.content.startswith("!문의") :
+             contents = message.content[4:]
+             username = str(message.author)
+             user = client.get_user(337849294591885322)
+             embed = discord.Embed(title="{} 님의 문의/건의내용".format(username), description = contents, colour= discord.Colour.gold())
+             await user.send(embed=embed)
+        else : 
+            await message.author.send("!문의 [내용] 으로 보내주세요.")
+    else :
+        if message.content.startswith("!문의") :
+            await message.channel.send("문의는 저에게 해주세요^^")
       
 client.run(access_token)
