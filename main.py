da4c8478ab5d603c3fa71df1f221703f82e127d0
kimdy03 <60499120+kimdy03@users.noreply.github.com>
2020-03-06 23:01:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,36 @@ import asyncio
 import discord
 import os
 from discord.ext import commands
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 client = commands.Bot(command_prefix='-')
+
+scope = [
+'https://spreadsheets.google.com/feeds',
+'https://www.googleapis.com/auth/drive',
+]
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name('/Users/user/Desktop/heroic-venture-270306-6b725eb305d4.json', scope)
+gc = gspread.authorize(credentials)
+spreadsheet_url = 'https://docs.google.com/spreadsheets/d/19lH3kuGI73vDO0jnfGWbCZLBNv9GlkMBtFEM5cOnmpk/edit#gid=0'
+
+doc = gc.open_by_url(spreadsheet_url)
+worksheet = doc.worksheet('JTB')
+userID = worksheet.col_values(1)
+
+def spread(id) :
+    if "{}".format(id) in userID :
+        for i in range(len(userID)) : 
+            if userID[i] == "{}".format(id) : 
+                temp = "B{}".format(i+1)
+                data = int(worksheet.acell(temp).value)
+                data += 1
+                date = str(data)
+                worksheet.update_acell(temp, date)
+    else : 
+        worksheet.update_acell("A{}".format(len(userID)+1), "{}".format(id))
+        worksheet.update_acell("B{}".format(len(userID)+1), "1")
 
 @client.event
 async def on_ready () : # 항상
@@ -14,6 +42,7 @@ async def on_ready () : # 항상
 contents = ""
 @client.event
 async def on_message(message) :
+    spread(int(message.author.id))
     if isinstance(message.channel,discord.DMChannel):
         if message.content.startswith("!문의") :
              contents = message.content[4:]
@@ -27,4 +56,4 @@ async def on_message(message) :
         if message.content.startswith("!문의") :
             await message.channel.send("문의는 저에게 해주세요^^")
       
-client.run(access_token)
+client.run(BOT_TOKEN)
