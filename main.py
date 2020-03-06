ae881225f60fe722564c70925f23598610748dd3
kimdy03 <60499120+kimdy03@users.noreply.github.com>
2020-03-06 22:58:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import discord
 import os
 from discord.ext import commands
 
+client = commands.Bot(command_prefix='-')
+
 @client.event
 async def on_ready () : # 항상
       print(client.user.name,"실행중")
