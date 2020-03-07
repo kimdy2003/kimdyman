5e558c6b1639c4df58066bc6032f9aae98d038c4
kimdy03 <60499120+kimdy03@users.noreply.github.com>
2020-03-07 23:24:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -30,8 +30,7 @@ def spread(id) :
                 date = str(data)
                 worksheet.update_acell(temp, date)
     else : 
-        worksheet.update_acell("A{}".format(len(userID)+2), "{}".format(id))
-        worksheet.update_acell("B{}".format(len(userID)+2), "1")
+        worksheet.insert_row(['{}'.format(id), '1'], len(userID)+1)
       
 @client.event
 async def on_ready () : # 항상
