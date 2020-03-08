961fea8e9acdcb3a8329f24a8211c95b35e5e5af
kimdy03 <60499120+kimdy03@users.noreply.github.com>
2020-03-08 13:47:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ async def on_ready () : # 항상
 contents = ""
 @client.event
 async def on_message(message) :
-    spread(message.author.id)
+    spread(int(message.author.id))
     if isinstance(message.channel,discord.DMChannel):
         if message.content.startswith("!문의") :
              contents = message.content[4:]
