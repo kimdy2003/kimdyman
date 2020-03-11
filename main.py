58426d4ede5ce3a986fe353a66c153161dd7a104
kimdy03 <60499120+kimdy03@users.noreply.github.com>
2020-03-11 21:45:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -37,14 +37,30 @@ def spread(id) :
 @client.event
 async def on_ready () : # 항상
       print(client.user.name,"실행중")
-      activity = discord.Game(name="문의는 DM")
+      activity = discord.Game(name="!문의봇 으로 명령어를 확인하세요")
       await client.change_presence(status=discord.Status.online, activity=activity) # idle = 자리비움
 
+@client.command()
+async def 문의봇 (ctx, *args) : 
+    if len(args) == 0 :
+        await ctx.send ("""```css
+[!문의봇 업데이트] : 업데이트 내역을 확인할 수 있습니다.
+[!문의 <문의내용>] : (Only DM) 문의/건의를 보낼 수 있습니다.```""")
+    elif args[0] == '업데이트' :
+        await ctx.send ("""```cs
+2020년 3월 11일 업데이트 내역 (0.1)
+#1 '이번이 첫 릴리즈라서 업데이트가 아님'```""")
+        
 contents = ""
 @client.event
 async def on_message(message) :
     spread(int(message.author.id))
-    if message.content.startswith ("!문의") :
-        user = client.get_user(message.author.id)
-        await user.send ("현재 사용할 수 없는 기능입니다.")
+    if message.content.startswith("!문의") :
+        contents = message.content[4:]
+        username = str(message.author)
+        user = client.get_user(337849294591885322)
+        embed = discord.Embed(title = '{} 해당 내용이 전달되었습니다!'.format(':eyes:') ,description = '전달 내용 : {}'.format(contents))
+        await message.author.send(embed=embed)
+        embed2 = discord.Embed(title = '{}님으로부터의 문의/건의 내용'.format(username), description = '{}'.format(contents))
+        await user.send(embed=embed2)
 client.run(token)
