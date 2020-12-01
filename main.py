40071574cc7c6a5e13bba6c747e3093445b9aa60
kimdy03 <60499120+kimdy03@users.noreply.github.com>
2020-12-01 12:05:51 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import logging
+from telegram import Update, Bot
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+import time
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+api = "1468499223:AAF1-zRu-S_xp1zT0vWwmGHQ6KyemKMf_wk"
+bot = Bot(token = api)
+
+def main() : 
+    updater = Updater("1468499223:AAF1-zRu-S_xp1zT0vWwmGHQ6KyemKMf_wk", use_context=True)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, get_message))
+    dispatcher.add_handler(CommandHandler('upload', upload)) 
+    updater.start_polling()
+    updater.idle()  
+
+def get_message(update : Update, context: CallbackContext) :
+        global Text 
+        Text = update.message.text
+        update.message.reply_text(
+            "이 메세지를 업로드하실겁니까? 만약 그렇다면 /upload 라고 입력해주세요.\n"
+            "전달할 메세지 :\n"+Text
+        )
+
+def upload(update : Update, context : CallbackContext) :
+    if Text != None :
+        bot.send_message(chat_id = '@koreanagora', text= "#AGORA\n\n" + Text)
+
+if __name__ == '__main__':
+    main()
