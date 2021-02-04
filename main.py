25365eb63fcb4566bc896908b9182d7312d46115
kimdy2003 <playkimdy@gmail.com>
2021-02-04 18:33:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,28 +7,35 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-api = "1468499223:AAF1-zRu-S_xp1zT0vWwmGHQ6KyemKMf_wk"
-bot = telegram.Bot(token = api)
+bot = telegram.Bot(token = '1661961084:AAEsBr-JTf5UkeCmaJNjrM5Tk8aeEet6ebM')
 
 def main() : 
-    updater = Updater("1468499223:AAF1-zRu-S_xp1zT0vWwmGHQ6KyemKMf_wk", use_context=True)
+    updater = Updater("1661961084:AAEsBr-JTf5UkeCmaJNjrM5Tk8aeEet6ebM", use_context=True)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, get_message))
-    dispatcher.add_handler(CommandHandler('upload', upload)) 
     updater.start_polling()
     updater.idle()  
 
 def get_message(update : telegram.Update, context: CallbackContext) :
-        global Text 
-        Text = update.message.text
-        update.message.reply_text(
-            "이 메세지를 업로드하실겁니까? 만약 그렇다면 /upload 라고 입력해주세요.\n"
-            "전달할 메세지 :\n"+Text
-        )
-
-def upload(update : telegram.Update, context : CallbackContext) :
-    if Text != None :
-        bot.send_message(chat_id = '@koreanagora', text= "#AGORA\n\n" + Text)
+    if update.message.text != None :
+        bot.send_message(chat_id = -1001424041679, text= update.message.text)
+        update.message.reply_text("성공적으로 전송되었습니다.")
+        user = update.message.from_user
+        try :
+            fname = user.first_name
+        except :
+            fname = ""
+        try : 
+            lname = user.last_name
+        except :
+            lname = ""
+        try :
+            username = user.username
+        except :
+            username = ""
+        userid = user.id
+        contents = str(lname)+" "+str(fname)+"  @"+str(username)+" id:"+str(userid)+"\n\n"+update.message.text
+        bot.send_message(chat_id = -1001251638746, text= contents)
 
 if __name__ == '__main__':
     main()
